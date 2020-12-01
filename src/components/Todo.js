76fb13a1af7a7370a9af416091b2d7204ed04c7b
jon del toro <61124054+jdeltoro88@users.noreply.github.com>
2020-11-30 23:53:49 -0700

    added todo list

---
+++ src/components/Todo.js
@@ -0,0 +1,84 @@
+import React, { useState } from "react";
+
+function TodoItem({ todo, index, completeTodo, removeTodo }) {
+  return (
+    <div
+      className="todo"
+      style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}
+    >
+      {todo.text}
+      <div>
+        <button onClick={() => completeTodo(index)}>Complete</button>
+        <button onClick={() => removeTodo(index)}>x</button>
+      </div>
+    </div>
+  );
+}
+
+function TodoForm({ addTodo }) {
+  const [value, setValue] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="todo"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <button className="todo" onSubmit={handleSubmit}>
+        Add
+      </button>
+    </form>
+  );
+}
+
+export default function Todo() {
+  const [todos, setTodos] = useState([
+    { text: "task 1", isCompleted: false },
+    { text: "task 2", isCompleted: false },
+    { text: "task 3", isCompleted: false },
+  ]);
+
+  function addTodo(text) {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  }
+
+  const completeTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = true;
+    setTodos(newTodos);
+  };
+
+  const removeTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
+  return (
+    <div className="widget-todo container">
+      <h2>Todo List</h2>
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <TodoItem
+            key={index}
+            index={index}
+            todo={todo}
+            completeTodo={completeTodo}
+            removeTodo={removeTodo}
+          />
+        ))}
+        <TodoForm addTodo={addTodo} />
+      </div>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import Moods from './components/Moods';
 import Spinner from './components/Spinner';
 import Squares from './components/Squares';
 import Programmers from './components/Programmers';
+import TicTacToe from './components/TicTacToe';
+import Todo from './components/Todo';
 
 render(
   <>
@@ -16,6 +18,8 @@ render(
     <Input />
     <Squares />
     <Programmers />
+    <TicTacToe />
+    <Todo />
   </>
   ,
   document.querySelector('#root')
